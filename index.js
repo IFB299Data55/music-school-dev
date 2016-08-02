11c7d02db413c51e0fb1183654ec077705d7c250
japugaletra <rvanw9@hotmail.com>
2016-08-02 11:54:09 +1000

    Added 404 Error

--- index.js
+++ index.js
@@ -17,6 +17,10 @@ app.get('/dev', function(request, response) {
   response.render('pages/dev-info');
 });
 
+app.use(function(request, response) {
+	response.render('pages/404');
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
