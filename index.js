eeb3b08396eb8012f8db30a059317162644610ff
japugaletra <rvanw9@hotmail.com>
2016-08-27 12:17:37 +1000

    Added Angular App

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+	response.render('pages/index');
 });
 
 app.get('/dev', function(request, response) {
-  response.render('pages/dev-info');
+	response.render('pages/dev-info');
 });
 
 require('./ownerAPI.js').include(app);
@@ -25,5 +25,5 @@ app.use(function(request, response) {
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+	console.log('Node app is running on port', app.get('port'));
 });
