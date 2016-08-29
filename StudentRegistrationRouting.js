bc590f742c6a2f83a948410a1a4503ee2b3e84f4
japugaletra <rvanw9@hotmail.com>
2016-08-29 20:01:20 +1000

    Fixed backend post

--- StudentRegistrationRouting.js
+++ StudentRegistrationRouting.js
@@ -2,10 +2,12 @@ exports.include = (app) => {
 	app.get('/register/student/', function(request, response) {
 	  response.render('studentRegistration/index');
 	});
+
 	app.post('/register/student/', function(request, response) {
-	  //response.send('Student Registered');
-	  response.sendStatus('201');
-	  //response.sendStatus('500');
+		var student = request.body;
+		//response.send('Student Registered');
+		response.sendStatus('201');
+		//response.sendStatus('500');
 	});
 
 	app.get('/register/student/*', function(request, response) {
