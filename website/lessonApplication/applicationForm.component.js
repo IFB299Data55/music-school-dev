c41775dbcd91ff0c030b4d5fff442e5dba042581
rvanw9 <rvanw9@hotmail.com>
2016-08-31 07:49:01 +1000

    Created Page

---
+++ website/lessonApplication/applicationForm.component.js
@@ -0,0 +1,32 @@
+(function(app) {
+  app.ApplicationFormComponent =
+    ng.core.Component({
+      selector: 'register-form' ,
+      templateUrl: localPath+'views/applicationForm.component.ejs',
+      styleUrls: [localPath+'css/applicationForm.component.css']
+    })
+    .Class({
+      constructor: [
+        app.LessonApplicationService,
+        ng.router.Router,
+	      function(LessonApplicationService,Router) {
+          this.LessonApplicationService = LessonApplicationService;
+          this.Router = Router;
+          this.submitted = false;
+
+          this.Register = function() {
+            this.submitted = true;
+            //Send to registration Service
+            //then redirect
+            /*this.RegistrationService.AttemptRegistration(this.student).then(() => {
+              var link = ['/Confirmation'];
+              this.Router.navigate(link);
+            }).catch(() => {
+              this.submitted = false;
+              this.error = 'An error has occured. Please try again later';
+            });*/
+          }
+	      }
+      ]
+    });
+})(window.app || (window.app = {}));
