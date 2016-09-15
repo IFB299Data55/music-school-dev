524239287abac19bd026acc82f9f14ad7c608f54
Anneke Kotze <ms.anneke.kotze@gmail.com>
2016-09-15 10:29:49 +1000

    added some things for accepting/rejecting students
    still not functional

--- AcceptStudentsRouting.js
+++ AcceptStudentsRouting.js
@@ -14,12 +14,17 @@ exports.include = (app) => {
 			students: studentsResult
 		}
 
-		var getQuery = "SELECT lr.request_id as requestid, s.first_name as firstname, s.surname as lastname, TO_CHAR(s.dob,'YYYY-MM-DD') as dob, it.name as instrument, se.grade as grade FROM music_school.lesson_requests lr, music_school.students s, music_school.instrument_types it, music_school.student_experience se WHERE lr.student_id = s.student_id AND lr.inst_type_id = it.inst_type_id AND lr.approved_date IS NULL AND s.student_id = se.student_id AND se.inst_type_id = lr.inst_type_id";
+		var getQuery = "SELECT lr.id as requestid, s.first_name as firstname, s.last_name as lastname, "
+						+"TO_CHAR(s.dob,'YYYY-MM-DD') as dob, it.name as instrument, se.grade as grade "
+						+"FROM music_school.lesson_requests lr, music_school.students s, "
+						+"music_school.instrument_types it, music_school.student_experience se "
+						+"WHERE lr.student_id = s.id AND lr.inst_type_id = it.id "
+						+"AND lr.approved_date IS NULL AND s.id = se.student_id AND se.inst_type_id = lr.inst_type_id";
 
 		app.client.query(getQuery).on('row', function(row) {
 			studentsResult.push(row);
 		})
-		.on('end', function(){
+		.on('end', function() {
 			if (!response.headersSent) {
 				if (studentsResult.length > 0) {
 					response.send(result);
@@ -32,7 +37,42 @@ exports.include = (app) => {
 	});
 
 	app.post('/teacher/studentApplications/individual/', function(request, response) {
-		// do stuff
+		var requestID = request.query.requestID;
+		var accept = request.query.accept; // true or false
+		var result = {
+			status: true,
+		}
+
+		if (accept) {
+			var acceptQuery = "";
+
+			// set request status to 2
+
+			app.client.query(acceptQuery).on('error', function() {
+				result.status = false;
+				response.send(result);
+			})
+			.on('end', function() {
+				if (!response.headersSent) {
+					response.send(result);
+				}
+			})
+		} else {
+			var rejectQuery = "";
+
+			// leave request status on 1
+			// 
+
+			app.client.query(rejectQuery).on('error', function() {
+				result.status = false;
+				response.send(result);
+			})
+			.on('end', function() {
+				if (!response.headersSent) {
+					response.send(result);
+				}
+			})
+		}
 	});
 
 	app.get('/teacher/studentApplications/*', function(request, response) {
