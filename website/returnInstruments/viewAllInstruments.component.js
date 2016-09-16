15dd8d59af76fcf15d518227c245a767374204b8
Anneke Kotze <ms.anneke.kotze@gmail.com>
2016-09-16 19:51:52 +1000

    Added search functionality

--- website/returnInstruments/viewAllInstruments.component.js
+++ website/returnInstruments/viewAllInstruments.component.js
@@ -14,12 +14,16 @@
           this.Router = Router;
           
           this.instruments = [];
+          this.filteredInstruments = [];
+          this.filterText = '';
 
           this.GetInstruments = function() {
             this.ReturnInstrumentsService.GetInstruments()
               .then(response => {
                 if (!response.error) {
                   this.instruments = response.instruments;
+                  this.filteredInstruments = this.instruments;
+                  this.Filter('');
                 } else {
                   this.error = 'An error has occured. Please contact administration for further assitance.';
                 }
@@ -33,6 +37,19 @@
             var link = ['/individual', instrumentID];
             this.Router.navigate(link);
           }
+
+          this.Filter = function() {
+            this.filteredInstruments = [];
+            filterString = new RegExp(this.filterText, "gi");
+            for (var i = 0; i < this.instruments.length; i++) {
+              var instrument = this.instruments[i];
+              var name = instrument.firstname+" "+instrument.lastname;
+              if (name.match(filterString)) {
+                this.filteredInstruments.push(instrument);
+              }
+            }
+          }
+
 	      }
       ]
     });
