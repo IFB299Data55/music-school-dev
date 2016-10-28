caab29b4c31b2892b493c04a80476cbba1abcff2
brothwell <brothwell1982@gmail.com>
2016-10-28 20:24:52 +1000

    Final commit of my portfolio - Brendan Rothwell

---
+++ documentation/Personal Portfolios/Brendan/CopyingDataScript - ExtraColumnValue.sql
@@ -0,0 +1,9 @@
+﻿ALTER TABLE music_school.instrument_types ALTER COLUMN lesson_fee DROP NOT NULL;
+
+INSERT INTO music_school.instrument_types (id, name)
+SELECT inst_type_id::integer, name FROM music_school_backup.instrument_types;
+
+UPDATE music_school.instrument_types
+SET lesson_fee = 30.00;
+
+ALTER TABLE music_school.instrument_types ALTER COLUMN lesson_fee SET NOT NULL;
